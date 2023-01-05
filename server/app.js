49c3fb789165a99381d9185ed91a01260940c88e
Aryse Tansy <medictansy@gmail.com>
2023-01-05 14:42:25 -0500

    Config: Helmet Deploy

--- server/app.js
+++ server/app.js
@@ -30,6 +30,15 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.hsts());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
+// GOOD HERE
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
