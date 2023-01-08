57fcbb4805457e4ebfe5216e28eb0f669c750208
Aryse Tansy <medictansy@gmail.com>
2023-01-08 12:47:58 -0500

    REFACTOR: Seed db logic and api doc

--- server/db/seed.js
+++ server/db/seed.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const { User } = require("../models/users");
 const { Post } = require("../models/posts");
 
-
 const connection = async () => {
   mongoose.set("strictQuery", false);
   await mongoose
