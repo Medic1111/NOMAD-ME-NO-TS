c10fdccf59d3b2cdcf1b057aaa25d60fc4785be9
Aryse Tansy <medictansy@gmail.com>
2023-01-13 08:47:09 -0500

    JEST: Finish POSTS route

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -5,14 +5,6 @@ const { User } = require("../models/users");
 const { Post } = require("../models/posts");
 jest.setTimeout(20000);
 
-// UPVOTE POST 200
-// UPVOTE POST 401
-// UPVOTE POST 404
-// UPVOTE POST 401
-// GET LABEL POST 200
-// GET LABEL POST 404
-// GET LABEL POST 401 (check if val middleware)
-
 beforeAll(async () => {
   mongoose.set("strictQuery", false);
   await mongoose
@@ -440,7 +432,7 @@ describe("Test POST route PATCH /:id for", () => {
   });
 
   // FAIL 422
-  test.only("Fails editting a post with invalid token", async () => {
+  test("Fails editting a post with invalid token", async () => {
     let data = { title: 1 };
     await request(app)
       .patch(`/api/v1/posts/${postIdDel}`)
@@ -455,6 +447,158 @@ describe("Test POST route PATCH /:id for", () => {
         expect(serverRes.body).toBeTruthy();
         expect(serverRes.body).toEqual(expect.any(Object));
         expect(serverRes.body.message).toBeDefined();
+      });
+  });
+});
+
+describe("Test POST route PATCH /:id/up_vote for ", () => {
+  let mockUser;
+  let token;
+  let postIdDel;
+  let id;
+  beforeEach(async () => {
+    mockUser = await User.create({
+      username: "jestuser",
+      password: "jestuser",
+      email: "jestuser@any.com",
+      passwordConfirm: "jestuser",
+    });
+
+    await request(app)
+      .post("/api/v1/auth/login")
+      .send({ username: "jestuser", password: "jestuser" })
+      .then((serverRes) => {
+        id = serverRes.body.user.id;
+        token = serverRes.header["set-cookie"][0]
+          .slice(4)
+          .split(" ")[0]
+          .slice(0, -1);
+      });
+    const data = {
+      author: id,
+      url: "https://media.istockphoto.com/id/847714996/photo/miami-beach-florida-usa.jpg?s=612x612&w=0&k=20&c=lT0wFzLOav0uoA8-glWpps552IVbHZaXEGtEtMxjVM8=",
+      title: "TESTING API3",
+      content: "This is my API TEST VAR Post",
+    };
+    await request(app)
+      .post("/api/v1/posts")
+      .set("Authorization", `Bearer ${token}`)
+      .set("Accept", "application/json")
+      .send(data)
+      .expect(201)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        postIdDel = serverRes.body.id;
+      });
+  });
+
+  afterEach(async () => {
+    await User.findOneAndDelete({ username: "jestuser" })
+      .then(() => console.log("AFTER_ALL:deleted"))
+      .catch((err) => console.log(err));
+    await Post.findByIdAndDelete(postIdDel)
+      .then(() => console.log("Post deleted"))
+      .catch((err) => console.log(err));
+  });
+
+  test("Successful up_vote/down_vote a spec post", async () => {
+    let data = { username: "jestuser" };
+    await request(app)
+      .patch(`/api/v1/posts/${postIdDel}/up_vote`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Accept", "application/json")
+      .send(data)
+      .expect(200)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        expect(serverRes.body).toBeDefined();
+        expect(serverRes.body).not.toBeNull();
+        expect(serverRes.body).toBeTruthy();
+        expect(serverRes.body).toEqual(expect.any(Object));
+        expect(serverRes.body.voteCount).toEqual(1);
+      });
+  });
+
+  test("NOT Successful up_vote/down_vote a spec post with wrong username", async () => {
+    let data = { username: "jestuser3" };
+    await request(app)
+      .patch(`/api/v1/posts/${postIdDel}/up_vote`)
+      .set("Authorization", `Bearer ${token}`)
+      .set("Accept", "application/json")
+      .send(data)
+      .expect(500)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        expect(serverRes.body).toBeDefined();
+        expect(serverRes.body).not.toBeNull();
+        expect(serverRes.body).toBeTruthy();
+        expect(serverRes.body).toEqual(expect.any(Object));
+        expect(serverRes.body.status).toBeDefined();
+      });
+  });
+
+  test("NOT Successful up_vote/down_vote a spec post with invalid token", async () => {
+    let data = { username: "jestuser3" };
+    await request(app)
+      .patch(`/api/v1/posts/${postIdDel}/up_vote`)
+      .set(
+        "Authorization",
+        `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RpbmciLCJpYXQiOjE2NzMxMjEwMzcsImV4cCI6MTY3MzEyNDYzN30.SYWTgsXMHURZjT1QRXZHixtaIE4wKA1yqUh0AxNpOQ4`
+      )
+      .set("Accept", "application/json")
+      .send(data)
+      .expect(403)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        expect(serverRes.body).toBeDefined();
+        expect(serverRes.body).not.toBeNull();
+        expect(serverRes.body).toBeTruthy();
+        expect(serverRes.body).toEqual(expect.any(Object));
+        expect(serverRes.body.message).toBeDefined();
+      });
+  });
+});
+
+describe("Test POSTS route GET /:id?label=x for ", () => {
+  let mockUser;
+  beforeAll(async () => {
+    mockUser = await User.create({
+      username: "jestuser",
+      password: "jestuser",
+      email: "jestuser@any.com",
+      passwordConfirm: "jestuser",
+    });
+  });
+
+  afterAll(async () => {
+    await User.findOneAndDelete({ username: "jestuser" })
+      .then(() => console.log("AFTER_ALL:deleted"))
+      .catch((err) => console.log(err));
+  });
+
+  test("Successful GET a spec post with label", async () => {
+    await request(app)
+      .get(`/api/v1/posts?="blue"`)
+      .expect(200)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        expect(serverRes.body).toBeDefined();
+        expect(serverRes.body).not.toBeNull();
+        expect(serverRes.body).toBeTruthy();
+        expect(serverRes.body).toEqual(expect.any(Array));
+      });
+  });
+
+  test("NOT Successful GET a spec post with invalid label, gets ALL", async () => {
+    await request(app)
+      .get(`/api/v1/posts?="indigo"`)
+      .expect(200)
+      .expect("Content-type", /json/)
+      .then((serverRes) => {
+        expect(serverRes.body).toBeDefined();
+        expect(serverRes.body).not.toBeNull();
+        expect(serverRes.body).toBeTruthy();
+        expect(serverRes.body).toEqual(expect.any(Array));
       });
   });
 });
